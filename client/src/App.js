f6966aa7f112bdc56b26e071532facbe29daa006
foestauf <46908845+foestauf@users.noreply.github.com>
2020-04-28 17:06:29 -0400

    Navs

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss';
+import {Switch, BrowserRouter, Route} from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import about from "./components/about";
+import home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav variant="pills" defaultActiveKey="/" className="mr-auto">
+              <LinkContainer to="/"><Nav.Link><Button>Home</Button></Nav.Link></LinkContainer>{' '}
+              <LinkContainer to="/about"><Nav.Link><Button>About</Button></Nav.Link></LinkContainer>{' '}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route path="/about" component={about}/>
+          <Route exact path="/" component={home}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+export default function () {
+    return(
+<div>
+    <Helmet>
+        <title>EZ Beer: About</title>
+    </Helmet>
+    Nothing here yet!
+</div>
+    )
+}
