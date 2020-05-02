81111057d4739c932e31eae41593d9c2089b7440
foestauf <46908845+foestauf@users.noreply.github.com>
2020-05-01 20:03:58 -0400

    schema

--- server/routes/ezbeer.js
+++ server/routes/ezbeer.js
@@ -11,10 +11,15 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, mongoOptions);
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    firstName: {type: String, required: true, unqieu: true},
-    lastName: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     eMail: {type: String, required: true, unique: true},
     dateCreated: {type: Number, default: Date.now},
-    lastUsedDate: {type: Number, default: Date.now}
+    lastLogin: {type: Number, default: Date.now},
 });
+
+const recipeSchema = new Schema({
+    user_id: {type: String, required: true}
+})
+
 const User = mongoose.model("User", userSchema);
