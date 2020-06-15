2090d9d5c8bfd116dc31fdb2f0f9c18fa814afcc
foestauf <46908845+foestauf@users.noreply.github.com>
2020-06-14 21:12:40 -0400

    Linting

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,20 +1,21 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 // Create Schema
 const RecipeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    style: {
-        type: String
-    },
-    ingredients: {
-        type: Array
-    },
-    owner: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  style: {
+    type: String,
+  },
+  ingredients: {
+    type: Array,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
 });
-module.exports = Recipe = mongoose.model("recipes", RecipeSchema);
+module.exports = Recipe = mongoose.model('recipes', RecipeSchema);
