2e686280c1fabaea9b2f1fe934ce29e9ebbd36be
foestauf <46908845+foestauf@users.noreply.github.com>
2020-06-14 21:58:35 -0400

    Refactor

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,22 +1,23 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRETORKEY;
 
-module.exports = passport => {
-    passport.use(
-        new JwtStrategy(opts, (jwt_payload, done) => {
-            User.findById(jwt_payload.id)
-                .then(user => {
-                    if (user) {
-                        return done(null, user);
-                    }
-                    return done(null, false);
-                })
-                .catch(err => console.log(err));
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwtPayload, done) => {
+      User.findById(jwtPayload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
         })
-    );
+        .catch((err) => console.log(err));
+    }),
+  );
 };
